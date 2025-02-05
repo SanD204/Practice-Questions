faabf4e115d3473c807a7d3673aec1093fe7bdd8
Sanyog Dani <sanyogdani1967@gmail.com>
2025-02-06 02:18:58 +0530

    added pattern

--- patterns.cpp
+++ patterns.cpp
@@ -61,6 +61,66 @@ void p6(int n){
 	}
 }
 
+void p7(int n){
+     for(int i = 0;i<n;i++){
+        //space
+        for(int j=0 ; j<n-i-1;j++){
+            cout<<" ";
+        }
+
+        //star
+        for(int k =0 ; k< 2*i+1;k++){
+            cout<<"*";
+        }
+
+        //space
+        for(int j=0 ; j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout << endl;
+    }
+}
+
+void p8(int n){
+    for(int i = 0;i<n;i++){
+        //space
+        for(int j=0 ; j<i;j++){
+            cout<<" ";
+        }
+
+        //star
+        for(int k =0 ; k< 2*n-(2*i+1);k++){
+            cout<<"*";
+        }
+
+        //space
+        for(int j=0 ; j<i;j++){
+            cout<<" ";
+        }
+        cout << endl;
+    }
+}
+
+void p9(int n){
+    p7(n);
+    p8(n);
+}
+
+void p10(int n) {
+    for(int i = 1; i<=2*n-1;i++){
+        int stars = i;
+        if(i>n){
+            stars = 2*n-i;
+        }
+
+        for(int j = 1; j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+
 int main(){
     int a = 4, b = 4, n=5;
     cout<< endl;
@@ -75,6 +135,14 @@ int main(){
     p5(n);
     cout<< endl;
     p6(n);
+    cout<< endl;
+    p7(n);
+    cout<< endl;
+    p8(n);
+    cout<< endl;
+    p9(n);
+    cout<< endl;
+    p10(n);
     cout<< endl;
     return 0;
 }
